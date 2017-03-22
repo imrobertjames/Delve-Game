a38017cdc54bae2bff74cf877b8baeee2b6f2248
robert <robert@192.168.1.130>
2017-03-21 22:08:13 -1000

    randomized seed

--- src/dev/delve/game/worlds/generation/WorldGenerator.java
+++ src/dev/delve/game/worlds/generation/WorldGenerator.java
@@ -1,17 +1,15 @@
 package dev.delve.game.worlds.generation;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
+import java.util.Random;
 
 public class WorldGenerator {
 
 	private static int WIDTH = 512;
 	private static int HEIGHT = 512;
 	private static double FEATURE_SIZE = 24;
-
+	private static Random rand = new Random();
 	private static int[][] map_array = new int[512][512];
 	
 	
@@ -19,14 +17,14 @@ public class WorldGenerator {
 		throws IOException {
 			
 		
-		OpenSimplexNoise noise = new OpenSimplexNoise();
+		OpenSimplexNoise noise = new OpenSimplexNoise(rand.nextLong());
 		BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 		
 		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH; x++) {
 				double value = noise.eval(x / FEATURE_SIZE, y / FEATURE_SIZE, 0.0, 0.0);
 				int rgb = 0x010101 * (int)((value + 1) * 127.5);
-				image.setRGB(x, y, rgb);
+				//image.setRGB(x, y, rgb);
 				map_array[x][y] = Math.abs((int)(value * 10));
 				//System.out.println("X: " + x + " || Y: " + y + " || Value: " + Math.abs((int)(value * 10)));
 			}
